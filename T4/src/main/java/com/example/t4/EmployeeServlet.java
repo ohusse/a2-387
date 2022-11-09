855abfae19a2490a3cc085c77bffc932a6d9418c
ohusse <78609674+ohusse@users.noreply.github.com>
2022-11-09 15:13:12 -0500

    t4

---
+++ T4/src/main/java/com/example/t4/EmployeeServlet.java
@@ -0,0 +1,50 @@
+package com.example.t4;
+
+import com.example.t4.Bean.Employee;
+import com.example.t4.Dao.EmployeeDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class EmployeeServlet extends HttpServlet{
+
+    private static final long serialVersionUID = 1;
+    private EmployeeDAO employeeDao;
+
+    public void init() {
+        employeeDao = new EmployeeDAO();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+
+        Employee employee = new Employee();
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setUsername(username);
+        employee.setPassword(password);
+        employee.setContact(contact);
+        employee.setAddress(address);
+
+        try {
+            employeeDao.registerEmployee(employee);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("employeedetails.jsp");
+    }
+}
