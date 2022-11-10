ac82545cada5d416f563e2563fd10a3812153b31
ohusse <78609674+ohusse@users.noreply.github.com>
2022-11-10 13:37:55 -0500

    Update adminServlet.java

--- T4/src/main/java/com/example/t4/adminServlet.java
+++ T4/src/main/java/com/example/t4/adminServlet.java
@@ -1,10 +1,10 @@
 package com.example.t4;
-
 import com.example.t4.Dao.UniversityDao;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -12,7 +12,7 @@ import java.io.PrintWriter;
 import java.sql.ResultSet;
 
 @WebServlet(name = "adminServlet", value = "/adminServlet")
-public class Servlet extends javax.servlet.http.HttpServlet {
+public class adminServlet extends javax.servlet.http.HttpServlet {
     private UniversityDao admin;
 
     public void init() {
